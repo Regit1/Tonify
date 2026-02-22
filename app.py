3e0aa8bde3f078b7e72cfc3b7a950a75cb5eb3ee
Regit1 <ajhum26@gmail.com>
2026-02-21 22:53:45 -0800

    updated site

--- app.py
+++ app.py
@@ -237,4 +237,6 @@ def find_closest(selected_song):
 # Run server
 # ---------------------------
 if __name__ == "__main__":
-    app.run(debug=False)
+    import os
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port, debug=True)
